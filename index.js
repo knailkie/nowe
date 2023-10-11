66ee8ee7d630fa64602d1172a227eebfdd79c767
knailkie <nikospokojest@gmail.com>
2023-10-11 13:31:54 +0200

    sds

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const mongoString = "mongodb+srv://nikospokojest:KupaDupa123@knak.vithec9.mongodb.net/?retryWrites=true&w=majority";
 const { MongoClient } = require("mongodb");
 const client = MongoClient(mongoString);
+async function main() {
+await client.connect();
+await client.close();
+}
+main();
