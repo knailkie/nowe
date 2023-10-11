4ef031086ef65f6b417213730d4811fd79d42e63
knailkie <nikospokojest@gmail.com>
2023-10-11 13:57:05 +0200

    kiuuruer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoString = "mongodb+srv://nikospokojest:KupaDupa123@knak.vithec9.mongodb.net/?retryWrites=true&w=majority";
 const { MongoClient } = require("mongodb");
-const client = MongoClient(mongoString);
+const client = new MongoClient(mongoString);
 async function main() {
 await client.connect();
 await client.close();
